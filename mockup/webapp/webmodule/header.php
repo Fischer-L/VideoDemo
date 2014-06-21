5704191a0b26e7c3db9d689a9f2e83ac8e56b242
Fischer-L <movie1254v@gmail.com>
2014-06-21 17:28:24 +0800

    #UPDATE: Done with the header layout.

--- mockup/webapp/webmodule/header.php
+++ mockup/webapp/webmodule/header.php
@@ -1,12 +1,15 @@
-<div class="header">
-	<div class="header-bigPoster"></div>
-	<div class="header-controlBar">
-		<div class="header-controlBar-settingCtrl">
-			<div class="header-controlBar-settingCtrl-icon sty-cursor-pter"></div>
-			<ul class="header-controlBar-settingCtrl-menu lyt-none">
+<div class="header lyt-pos-rel">
+	<div class="header-bigPoster">
+	</div>
+	<div class="header-controlBar lyt-pos-rel">
+		<div class="header-controlBar-settingCtrl lyt-pos-rel sty-cursor-pter">
+			<div class="header-controlBar-settingCtrl-iconWrp lyt-pos-rel">
+				<div class="header-controlBar-settingCtrl-icon"></div>
+			</div>
+			<ul class="header-controlBar-settingCtrl-menu lyt-pos-abs lyt-txtAlign-ctr">
 				<li class="header-controlBar-settingCtrl-option settingCtrl-member">Member center</li>
-				<li class="header-controlBar-settingCtrl-option settingCtrl-signin lyt-none">Sign in</li>
-				<li class="header-controlBar-settingCtrl-option settingCtrl-signout lyt-none">Sign out</li>
+				<li class="header-controlBar-settingCtrl-option settingCtrl-signin">Sign in</li>
+				<li class="header-controlBar-settingCtrl-option settingCtrl-signout">Sign out</li>
 			</ul>
 		</div>
 		<div class="header-controlBar-srchCtrl">
